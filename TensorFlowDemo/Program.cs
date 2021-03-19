311b1c8d6500ce8a021b09f036fc88c5a46df653
Daniel Basler <daniel.basler@web.de>
2021-03-19 08:45:03 +0100

    Add project files.

---
+++ TensorFlowDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Tensorflow.Binding;
+using System.Text;
+
+namespace TensorFlowDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            var hallo = tf.constant("Hallo, TensorFlow!");
+
+            using (var sess = tf.Session())
+            {
+                var res = sess.run(hallo);                
+                print(sess.run(hallo));
+
+                string decoded = ascii.GetString(res.ToByteArray());                
+                print(decoded);
+
+            }            
+        }
+    }
+}
